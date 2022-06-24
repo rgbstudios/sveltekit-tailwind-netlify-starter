5c33ac2e574c1c328da6ffa4d46ee117a7e67de8
Justin Golden <justingolden21@gmail.com>
2022-06-24 01:20:01 -0700

    Add tailwind typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Open Sans', ...defaultTheme.fontFamily.sans],
+				display: ['Poppins'],
+				body: ['Open Sans']
+			}
+		}
 	},
-	plugins: [require('daisyui')]
+	plugins: [require('daisyui'), require('@tailwindcss/typography')],
 };
